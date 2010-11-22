01664f4d4e95a8e6d320f41a545d263b68eee145
Milktrader <milktrader@gmail.com>
2010-11-21 22:56:53 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleZoo::Application.config.secret_token = 'df50809220cbea8eea68d17926540d44f52d7a82600ae8f411282f628ad830ed7fc493664a2fdcc7c5a79df39b72b226c5adbe7ba51fd9fcc985f05b7080b062'
