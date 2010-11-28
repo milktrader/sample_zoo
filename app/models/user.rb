fffb5c1d1377e10dc3b4ada200413cbf6f001097
Milktrader <milktrader@gmail.com>
2010-11-28 00:11:21 -0500

    user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,15 @@
 #  updated_at :datetime
 #
 
+
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :twitter
+  attr_accessible :name, :email #, :twitter
+  
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name,  :presence   => true,  
+                    :length     => { :maximum => 50 } 
+  validates :email, :presence   => true,
+                    :format     => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false}
 end
