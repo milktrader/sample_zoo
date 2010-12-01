bf9bee0fbe661330f2c787a45d4ad6c7382ce454
Milktrader <milktrader@gmail.com>
2010-11-30 23:02:36 -0500

    pwd and confirm not working not sure why

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,15 +9,16 @@ class UsersController < ApplicationController
     @user = User.new
     @title = "Sign up"
   end
-
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #handle save
+    else
+      @title = "Sign up"
+      render 'new' 
+    end   
+  end
   
 
 end
-# class UsersController < ApplicationController
-# 
-#   def show
-#     @user = User.find(params[:id])
-#   end
-#   
-#   
-# end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -54,4 +54,32 @@ describe UsersController do
       response.should have_selector('title', :content => "Sign up" )      
     end
   end
+  
+  describe "POST 'create'" do
+    describe "failure" do
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "", :password_confirmation => ""  }
+      end
+      
+      it "should have the correct title" do
+        post :create, :user => @attr  
+        response.should have_selector('title', :content => "Sign up")
+      end
+      
+      it "should render the 'new' page" do
+        post :create, :user => @attr  
+        response.should render_template('new')
+      
+      end
+          
+      it "should not create a new user" do
+        lambda do
+          post :create, :user => @attr  
+      end.should_not change(User, :count)
+    end
+            
+      
+    end
+  end
+  # the last end
 end
