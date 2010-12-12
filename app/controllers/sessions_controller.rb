92fb264ca8791197e88155e191fbd8a08b1674fc
Milktrader <milktrader@gmail.com>
2010-12-11 19:29:04 -0500

    new stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,19 @@ class SessionsController < ApplicationController
   def new
     @title = "Sign in"
   end
+  
   def create
-    
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])                    
+    if user.nil?
+      flash.now[:error] = "Invalid email/password combo, bro"
+      @title ="Sign in"
+      render 'new'
+    else
+      # handle successful sigin
+    end
   end
-  def destroy
-    
+  
+  def destroy    
   end
 end
